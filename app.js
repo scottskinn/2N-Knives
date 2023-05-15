a996eceb16a23a245ea008f40916552623f2aa35
scottskinn <84105761+scottskinn@users.noreply.github.com>
2023-05-15 15:07:00 -0400

    update home page links

--- app.js
+++ app.js
@@ -1,28 +1,45 @@
-let slideIndex = [1, 1];
-let slideId = ["mySlides1", "mySlides2", "mySlides2"]
-showSlides(1, 0);
-showSlides(1, 1);
+// let slideIndex = [1, 1];
+// let slideId = ["mySlides1", "mySlides2", "mySlides2"]
+// showSlides(1, 0);
+// showSlides(1, 1);
 
-function plusSlides(n, no) {
-    showSlides(slideIndex[no] += n, no);
-}
+// function plusSlides(n, no) {
+//     showSlides(slideIndex[no] += n, no);
+// }
 
-// Thumbnail image controls
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
+// // Thumbnail image controls
+// function currentSlide(n) {
+//     showSlides(slideIndex = n);
+// }
 
-function showSlides(n, no) {
-    let i;
-    let x = document.getElementsByClassName(slideId[no]);
-    let dots = document.getElementsByClassName("dot");
-    if (n > x.length) { slideIndex[no] = 1 }
-    if (n < 1) { slideIndex[no] = x.length }
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    x[slideIndex[no] - 1].style.display = "block";
-}
+// function showSlides(n, no) {
+//     let i;
+//     let x = document.getElementsByClassName(slideId[no]);
+//     let dots = document.getElementsByClassName("dot");
+//     if (n > x.length) { slideIndex[no] = 1 }
+//     if (n < 1) { slideIndex[no] = x.length }
+//     for (i = 0; i < x.length; i++) {
+//         x[i].style.display = "none";
+//     }
+//     for (i = 0; i < dots.length; i++) {
+//         dots[i].className = dots[i].className.replace(" active", "");
+//     }
+//     x[slideIndex[no] - 1].style.display = "block";
+// }
+
+// contact page
+
+document.getElementById("contact-form").addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var message = document.getElementById("message").value;
+
+    var subject = "Contact Form Submission from " + name;
+    var body = "Email: " + email + "\n\nMessage: " + message;
+
+    var mailtoLink = "mailto:twonknives@gmail.com?subject=" + encodeURIComponent(subject) + "&body=" + encodeURIComponent(body);
+
+    window.location.href = mailtoLink;
+});
